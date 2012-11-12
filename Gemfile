56c463bea5a9d018ea146c904c74309bc5d3126e
AvnerCohen <israbirding@gmail.com>
2012-11-12 22:46:23 +0200

    That was simpler than expected... working sinatra app searching through solr

--- Gemfile
+++ Gemfile
@@ -1,2 +1,9 @@
 source 'https://rubygems.org'
+
+
+#the solr client
+gem 'rsolr'
+
+
+# For sinatra
 gem "sinatra", :require => "sinatra/base"
--- birds_search.rb
+++ birds_search.rb
@@ -1,8 +1,22 @@
 class BirdsSearchApp < Sinatra::Base
+
+   configure :production, :development do
+    enable :logging
+  end
+
   set :static, true
   set :root, File.dirname(__FILE__)
 
   get "/" do
     redirect "index.html"
+  end
+
+
+  #Solar routeså
+  solr = RSolr.connect :url => 'http://localhost:8983/solr'
+
+  get "/search/:search" do |search|
+   page=params["page"].to_s
+   solr.get 'select', :params => {:q => "title_t:"+ search, :wt => :json, :start=> page }
   end
 end
